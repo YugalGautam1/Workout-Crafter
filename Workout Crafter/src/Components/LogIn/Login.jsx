5b92e6e876aa35617cdf14f954efe5797225259b
YugalGautam1 <reachyugal@gmail.com>
2024-05-16 01:54:08 -0400

    Set up Sub Optimal, Whatver need to do routing and set up the main page

--- Workout Crafter/src/Components/LogIn/Login.jsx
+++ Workout Crafter/src/Components/LogIn/Login.jsx
@@ -1,11 +1,43 @@
 import React,{useState, useEffect} from 'react'
 import './Login.css'
 
-export const Login = () => {
+ const Login = () => {
     const [data,setData] =  useState({username:'',password:''})
     
-    const handleSubmit = (e)->{
+
+    const handleChange= (e)=>{
+        const{name,value} = e.target
+        setData((previousData)=>{
+            return{
+                ...previousData,
+                [name]:value
+
+            }
+            
+        })
+    }
+    const handleSubmit = async (e)=>{//Some guy told me this is stupid and I should use formdata Look into that w
         e.preventDefault();
+        try{
+            const response =  await fetch('http://localhost:5000/login',
+            {
+                method: 'POST', 
+                headers: {
+                    'Content-Type':'application/json',
+                },
+                body: JSON.stringify(data),
+            })
+            if(response.ok){
+                console.log("Works")
+            }
+            else{
+                console.log("WRONG!")
+            }
+        }
+        catch(er){
+            console.log(er)
+
+        }        
         
     }
 
@@ -19,14 +51,14 @@ return (
         <div className = "head">
         Login
     
-        <form method = "POST">
+        <form onSubmit={handleSubmit}>
         Username: 
         <label htmlFor = "username"></label>
-        <input type = "text" id = "username"></input>
+        <input type = "text" name = "username" value = {data.username} id = "username" onChange = {handleChange}></input>
         <br />
         Password:  
         <label htmlFor = "password"></label>
-        <input type = "text" id = "password"></input>
+        <input type = "text" name = "password" id = "password" value = {data.password} onChange = {handleChange}></input>
         <br/>
         <button type = "submit">Submit</button>
         </form>
--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/login": {"origins": "*"}})
 
-#APi route
-
-@app.route("/login")
-
+@app.route('/login', methods=['POST'])
 def login():
-    return{"login":["Login,Login2"]}
+    login_data= request.get_json()
+    username = login_data.get('username')
+    password = login_data.get('password')
+    if(username =='User' and password == "Pass"):
+        message = {'message': 'Login Successful'}
 
+        return jsonify(message),200
+    else:
+        message = {'message': 'Login Invalid'}
+
+        return jsonify(message),400
 if __name__ == "__main__":
     app.run(debug=True)
